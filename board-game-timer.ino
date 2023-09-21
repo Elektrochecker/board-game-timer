b890e11f7052a35ecf2b4aa95eb2ff73ed40fc55
Timon Lilje <timon.lilje@gmail.com>
2023-09-21 21:30:05 +0200

    timer

--- board-game-timer.ino
+++ board-game-timer.ino
@@ -1,44 +1,60 @@
 //A0 - A5 work as digital pins
-int pins[12] =  {2,3,13,4,6,5,7,8,9,10,11,12};
+const int pin[12] =  {2,3,13,4,6,5,7,8,9,10,11,12};
 
 void setup() {
   Serial.begin(115200);
 
   for (int i = 0; i < 12; i++) {
-    pinMode(pins[i], OUTPUT);
+    pinMode(pin[i], OUTPUT);
   }
+
+  startupAnimation();
 }
 
 void loop() {
+  wait(60);
+  delay(1000);
+}
+
+void wait(float secs) {
+  float t = secs * 1000;
+
+  for (int i = 0; i < 12; i++) {
+    show(12 - i);
+    delay(t / 12);
+  }
+}
+
+void startupAnimation() {
   for (int i = 0; i <= 12; i++) {
     show(i);
-    delay(100);
+    delay(50);
   }
 
   for (int i = 12; i >= 0; i--) {
     show(i);
-    delay(100);
+    delay(50);
   }
 }
-
+//RBG1200_YBG1200#KIN
 //verbundene LED segmente:
 //2-3, 5-6, 7-8, 10-11
 void show(int p) {
   if (p > 12) p = 12;
   if (p < 0)  p = 0;
 
-  int led[12] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
+  int state[12] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
 
   for (int i = 0; i < p; i++) {
-    led[i] = HIGH;
+    state[i] = HIGH;
   }
 
-  if (p > 2)  led [1] = LOW;
-  if (p > 5)  led [4] = LOW;
-  if (p > 7)  led [6] = LOW;
-  if (p > 10) led [9] = LOW;
+  if (p > 2)  state [1] = LOW;
+  if (p > 5)  state [4] = LOW;
+  if (p > 7)  state [6] = LOW;
+  if (p > 10) state [9] = LOW;
 
   for (int i = 0; i < 12; i++) {
-    digitalWrite(pins[i], led[i]);
+    digitalWrite(pin[i], state[i]);
   }
 }
